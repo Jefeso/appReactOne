6ac67add71b15e8e8aa65c6e94e3ee46b66cb773
unknown <Jefeso>
2026-04-03 20:21:47 -0300

    atualizar carrinho

--- app/(drawer)/carrinho.tsx
+++ app/(drawer)/carrinho.tsx
@@ -16,7 +16,7 @@ export default function Carrinho() {
                 data={carrinho}
                 keyExtractor={item => item.id}
                 contentContainerStyle={{ padding: 12, paddingBottom: 160}}
-                renderItem{({ item }) => (
+                renderItem={({ item }) => (
                     <View style={s.card}>
                         <Image source={{ uri: item.imagem }} style={s.img} /> 
                         <View style={s.info}>
@@ -48,7 +48,7 @@ export default function Carrinho() {
             />
         )}
 
-        {carrinho.length > 0 && (}
+        {carrinho.length > 0 && (
             <View style={s.rodape}>
                 <Text style={s.totalLabel}>Total:</Text>
                 <Text style={s.totalValor}>R$ {total.toFixed(2)}</Text>
@@ -63,3 +63,74 @@ export default function Carrinho() {
 
 const s = StyleSheet.create({
     vazio: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+    vazioTxt: { color: '#aaa', fontSize: 18 },
+    card: {
+        flexDirection:'row',
+        backgroundColor: '#1e0a35',
+        borderRadius: 10,
+        marginBottom: 12,
+        overflow:'hidden',
+        borderWidth: 1,
+        borderColor: '#4a1a6e'
+    },
+
+    img: { width: 100, 
+        height:100
+     },
+
+     info:{
+        flex:1
+     },
+
+     titulo: {
+        color: 'white',
+     },
+
+    preco: {
+    color: '#78ff78',
+    },
+
+    subtotal: {
+        color: '#ccc'
+    },
+
+    controles: {
+        flexDirection: 'row',
+    },
+
+    btn: {
+        backgroundColor: '#4a1a6e',
+    },
+
+    btnRemover: {
+        backgroundColor: '#6e1a1a',
+    },
+
+    btnTxt: {
+        color: 'white',
+    },
+
+    quantidade: {
+        color: 'white',
+    },
+
+    rodape: {
+        position: 'absolute',
+    },
+
+    totallabel: {
+        color: '#aaa',
+    },
+
+    totalValor: {
+        color: '#white',
+    },
+
+    btnFinalizar: {
+        backgroundColor: '#7b2dff'
+    },
+
+    btnFlinalizarTxt: {
+        color: 'white',
+    },
+})
